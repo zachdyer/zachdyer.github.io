2e644ce512216ffd780ac128b4a3575f828a0b53
Zach Dyer <zach.dyer@zachdyerdesign.com>
2016-11-14 17:32:30 -0600

    chat

--- js/zachdyerdesign/chat.js
+++ js/zachdyerdesign/chat.js
@@ -1,26 +1,47 @@
 var zachdyerdesign = zachdyerdesign || {};
 
 zachdyerdesign.chat = {
+  chatWindow: null,
+  input: null,
+  output: null,
   createWindow: function (width, height) {
     var tab = document.createElement("div");
-    var chat = document.createElement("div");
-    var input = document.createElement("input");
-
-    chat.setAttribute("class", "chat");
     tab.setAttribute("class", "tab");
-    input.setAttribute("class", "form-control input");
-    input.setAttribute("type", "text");
-
-    chat.style.width = width + "px";
-    chat.style.height = height + "px";
-
     tab.style.width = width + "px";
 
-    chat.appendChild(tab);
-    chat.appendChild(input);
-    document.body.appendChild(chat);
+    this.output = document.createElement("div");
+    this.output.setAttribute("class", "output");
+
+    this.input = document.createElement("input");
+    this.input.setAttribute("class", "form-control input");
+    this.input.setAttribute("type", "text");
+
+    this.chatWindow = document.createElement("div");
+    this.chatWindow.setAttribute("class", "chat");
+    this.chatWindow.style.width = width + "px";
+    this.chatWindow.style.height = height + "px";
+    this.chatWindow.appendChild(tab);
+    this.chatWindow.appendChild(this.input);
+    this.chatWindow.appendChild(this.output);
+
+    document.body.appendChild(this.chatWindow);
   },
   build: function () {
     this.createWindow(250, 300);
+
+    window.addEventListener("keypress", function (evt) {
+      var self = zachdyerdesign.chat;
+      if(evt.keyCode == 13) {
+        console.log(self.input.value);
+        let input = document.createTextNode(self.input.value);
+        let br = document.createElement("br");
+        self.chatWindow.appendChild(input);
+        self.chatWindow.appendChild(br);
+        self.clear();
+      }
+    });
+  },
+  clear: function () {
+    this.input.value = "";
   }
 };
