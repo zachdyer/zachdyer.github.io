e479b6cc31c664decbb3c9fca80a060dc2eb2644
Zach Dyer <zach.dyer@zachdyerdesign.com>
2016-11-14 20:59:41 -0600

    chat plugin

--- js/zachdyerdesign/chat.js
+++ js/zachdyerdesign/chat.js
@@ -4,10 +4,12 @@ zachdyerdesign.chat = {
   chatWindow: null,
   input: null,
   output: null,
+  tab: null,
+  minimize: false,
   createWindow: function (width, height) {
-    var tab = document.createElement("div");
-    tab.setAttribute("class", "tab");
-    tab.style.width = width + "px";
+    this.tab = document.createElement("div");
+    this.tab.setAttribute("class", "tab");
+    this.tab.style.width = width + "px";
 
     this.output = document.createElement("div");
     this.output.setAttribute("class", "output");
@@ -20,28 +22,54 @@ zachdyerdesign.chat = {
     this.chatWindow.setAttribute("class", "chat");
     this.chatWindow.style.width = width + "px";
     this.chatWindow.style.height = height + "px";
-    this.chatWindow.appendChild(tab);
+
+    this.chatWindow.appendChild(this.tab);
     this.chatWindow.appendChild(this.input);
     this.chatWindow.appendChild(this.output);
-
     document.body.appendChild(this.chatWindow);
   },
   build: function () {
     this.createWindow(250, 300);
+    this.createListeners();
 
-    window.addEventListener("keypress", function (evt) {
-      var self = zachdyerdesign.chat;
-      if(evt.keyCode == 13) {
-        console.log(self.input.value);
-        let input = document.createTextNode(self.input.value);
-        let br = document.createElement("br");
-        self.chatWindow.appendChild(input);
-        self.chatWindow.appendChild(br);
-        self.clear();
-      }
-    });
   },
   clear: function () {
     this.input.value = "";
+  },
+  draw: function () {
+    let input = document.createTextNode(this.input.value);
+    let br = document.createElement("br");
+    this.output.appendChild(input);
+    this.output.appendChild(br);
+    this.scrollDown();
+  },
+  createListeners: function () {
+    var self = zachdyerdesign.chat;
+
+    window.addEventListener("keypress", function (evt) {
+      if(evt.keyCode == 13) {
+        self.draw();
+        self.clear();
+      }
+    });
+
+    this.tab.addEventListener("click", function () {
+      console.log("click");
+      if(self.minimize) {
+        self.output.style.display = "block";
+        self.input.style.display = "block";
+        self.chatWindow.style.height = "300px";
+        self.scrollDown();
+        self.minimize = false;
+      } else {
+        self.output.style.display = "none";
+        self.input.style.display = "none";
+        self.chatWindow.style.height = "30px";
+        self.minimize = true;
+      }
+    });
+  },
+  scrollDown: function () {
+    this.output.scrollTop = this.output.scrollHeight;
   }
 };
